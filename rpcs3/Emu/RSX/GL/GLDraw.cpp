c2e500ffd49839914f126741f4d8515ebd42cf59
( ง ᵒ̌皿ᵒ̌)ง⁼³₌₃ <hoholee12@naver.com>
2022-06-05 23:17:10 +0900

    bump to v6

--- rpcs3/Emu/RSX/GL/GLDraw.cpp
+++ rpcs3/Emu/RSX/GL/GLDraw.cpp
@@ -631,7 +631,6 @@ void GLGSRender::end()
 	if (!load_program())
 	{
 		// Program is not ready, skip drawing this
-		std::this_thread::yield();
 		execute_nop_draw();
 		// m_rtts.on_write(); - breaks games for obvious reasons
 		rsx::thread::end();
