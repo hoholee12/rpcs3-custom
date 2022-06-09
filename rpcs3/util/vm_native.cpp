17d3c52421b6a7234cae0ddda1968667ae793a59
( ง ᵒ̌皿ᵒ̌)ง⁼³₌₃ <hoholee12@naver.com>
2022-06-10 01:55:15 +0900

    dont throw when virtualprotect fails

--- rpcs3/util/vm_native.cpp
+++ rpcs3/util/vm_native.cpp
@@ -275,7 +275,7 @@ namespace utils
 
 			if (!::VirtualProtect(reinterpret_cast<LPVOID>(addr), block_size, +prot, &old))
 			{
-				fmt::throw_exception("VirtualProtect failed (%p, 0x%x, addr=0x%x, error=%#x)", pointer, size, addr, GetLastError());
+				//fmt::throw_exception("VirtualProtect failed (%p, 0x%x, addr=0x%x, error=%#x)", pointer, size, addr, GetLastError());
 			}
 
 			// Next region
