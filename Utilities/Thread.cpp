16d18418cc8505fe5b858e8919e1f485845064c5
( ง ᵒ̌皿ᵒ̌)ง⁼³₌₃ <hoholee12@naver.com>
2023-04-10 02:08:33 +0900

    test

--- Utilities/Thread.cpp
+++ Utilities/Thread.cpp
@@ -2713,9 +2713,9 @@ u64 thread_ctrl::get_affinity_mask(thread_class group)
 					//too few physical cores
 					switch (group)
 					{
-					case thread_class::sha: return all_cores_mask & 0b11;
+					case thread_class::sha: 
 					case thread_class::rsx: return all_cores_mask & 0b1111; //limit to 2 physical cores
-					case thread_class::rec: return all_cores_mask & (ipow(2, thread_count) - 16);
+					case thread_class::rec: 
 					case thread_class::spu: return all_cores_mask & (ipow(2, thread_count) - 4); //pin out one core
 					case thread_class::ppu:
 					default: return all_cores_mask;
@@ -2742,9 +2742,9 @@ u64 thread_ctrl::get_affinity_mask(thread_class group)
 					//too few physical cores
 					switch (group)
 					{
-					case thread_class::sha: return all_cores_mask & 0b1;
+					case thread_class::sha: 
 					case thread_class::rsx: return all_cores_mask & 0b11; //limit to 2 cores
-					case thread_class::rec: return all_cores_mask & (ipow(2, thread_count) - 4);
+					case thread_class::rec: 
 					case thread_class::spu: return all_cores_mask & (ipow(2, thread_count) - 2); //pin out one core
 					case thread_class::ppu:
 					default: return all_cores_mask;
@@ -2790,9 +2790,9 @@ u64 thread_ctrl::get_recommended_workercount(thread_class group)
 					//too few physical cores
 					switch (group)
 					{
-					case thread_class::sha: return 2;
+					case thread_class::sha: 
 					case thread_class::rsx: return 4; //limit to 2 physical cores
-					case thread_class::rec: return thread_count - 4;
+					case thread_class::rec: 
 					case thread_class::spu: return thread_count - 2; //pin out one core
 					case thread_class::ppu:
 					default: return thread_count;
@@ -2819,9 +2819,9 @@ u64 thread_ctrl::get_recommended_workercount(thread_class group)
 					//too few physical cores
 					switch (group)
 					{
-					case thread_class::sha: return 1;
+					case thread_class::sha: 
 					case thread_class::rsx: return 2; //limit to 2 cores
-					case thread_class::rec: return thread_count - 2;
+					case thread_class::rec: 
 					case thread_class::spu: return thread_count - 1; //pin out one core
 					case thread_class::ppu:
 					default: return thread_count;
